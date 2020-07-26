8a409d1bad5d4d8bb3cd30196e31dd87c5e91291
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-07-26 09:08:59 -0300

    Create component 'powered-by'

---
+++ src/components/PoweredBy.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer class="powered-by">
+    <span v-if="showTextSpan">
+      Powered by
+    </span>
+    <a href="https://trakt.tv/" class="d-flex">
+      <img
+        height="20px"
+        :src="require('@/assets/trakt-wide-red-black.svg')"
+        alt="Logo Trakt"
+      >
+    </a>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      sheet: false,
+      showTextSpan: window.screen.width > 375,
+    };
+  },
+};
+</script>
+<style>
+  .powered-by span {
+    margin-right: 6px;
+  }
+  .powered-by a {
+    display: flex;
+  }
+  .powered-by {
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+    color: gray;
+  }
+</style>
