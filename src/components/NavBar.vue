8ec9bdf7c09a3a24e574aeca959676228f44589e
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-07-26 09:11:35 -0300

    Update navigation bar with search route and powered-by component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,45 @@
 <template>
-  <v-bottom-navigation
-    v-model="nav"
-    app
-    color="primary"
-  >
-    <v-btn value="home">
-      <home-icon />
-    </v-btn>
+  <div>
+    <div
+      class="powered-by-wrapper"
+    >
+      <powered-by class="mt-3"/>
+    </div>
+    <v-bottom-navigation
+      v-model="nav"
+      app
+      height="4.5rem"
+      color="primary"
+    >
+      <v-btn
+        id="home"
+        :to="HOME.PATH"
+        :value="HOME.PATH"
+      >
+        <home-icon />
+      </v-btn>
 
-    <v-btn value="discovery">
-      <search-icon />
-    </v-btn>
+      <v-btn
+        id="discovery"
+        :to="SEARCH.PATH"
+        :value="SEARCH.PATH"
+      >
+        <search-icon />
+      </v-btn>
 
-    <v-btn value="dashboard">
-      <grid-icon />
-    </v-btn>
+      <v-btn value="dashboard">
+        <grid-icon />
+      </v-btn>
 
-    <v-btn value="calendar">
-      <calendar-icon />
-    </v-btn>
+      <v-btn value="calendar">
+        <calendar-icon />
+      </v-btn>
 
-    <v-btn value="watched">
-      <archive-icon />
-    </v-btn>
-  </v-bottom-navigation>
+      <v-btn value="watched">
+        <archive-icon />
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
 </template>
 
 <script>
@@ -34,6 +50,11 @@ import {
   CalendarIcon,
   ArchiveIcon,
 } from 'vue-feather-icons';
+import PoweredBy from './PoweredBy.vue';
+import {
+  HOME,
+  SEARCH,
+} from '../constants/routes';
 
 export default {
   components: {
@@ -42,14 +63,29 @@ export default {
     GridIcon,
     CalendarIcon,
     ArchiveIcon,
+    PoweredBy,
   },
 
   name: 'NavBar',
 
   data() {
     return {
-      nav: 'home',
+      nav: this.$router.currentRoute.path,
+      HOME,
+      SEARCH,
     };
   },
 };
 </script>
+
+<style lang="sass" scoped>
+$a: 255
+.powered-by-wrapper
+  position: fixed
+  bottom: 65px
+  right: 0
+  left: -10px
+  width: 100%
+  background: white
+  background: linear-gradient(0deg,rgba($a,$a,$a,1) 0%,rgba($a,$a,$a,1) 85%,rgba($a,$a,$a,0) 100%)
+</style>
