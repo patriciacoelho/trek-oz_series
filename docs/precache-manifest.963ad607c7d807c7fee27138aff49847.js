9ea95fd04a861c1a19021042280bd79bfdd21e37
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-07-28 21:15:51 -0300

    Upload docs folder

---
+++ docs/precache-manifest.963ad607c7d807c7fee27138aff49847.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8324974784bdc1fcdd",
+    "url": "/trek-oz_series/css/app.d3707e63.css"
+  },
+  {
+    "revision": "8d59e1ccb2e346acbb0b",
+    "url": "/trek-oz_series/css/chunk-2c8b44e9.c5267e8a.css"
+  },
+  {
+    "revision": "d31e1b2be5f968db6ea4",
+    "url": "/trek-oz_series/css/chunk-8af573d2.ea792eba.css"
+  },
+  {
+    "revision": "60747180cb7fcbe2cc05",
+    "url": "/trek-oz_series/css/chunk-9b4cece0.4ec7fb8f.css"
+  },
+  {
+    "revision": "7ff63135c0cb7e4b15f6",
+    "url": "/trek-oz_series/css/chunk-vendors.f71dadeb.css"
+  },
+  {
+    "revision": "8eae3513c245721f527070ea1aff3c9f",
+    "url": "/trek-oz_series/img/offline.svg"
+  },
+  {
+    "revision": "e5b42e7599b0efe2b6d2d74e6a6defd3",
+    "url": "/trek-oz_series/img/trakt-wide-red-black.e5b42e75.svg"
+  },
+  {
+    "revision": "933c4848eba222751bae63f143234f1b",
+    "url": "/trek-oz_series/index.html"
+  },
+  {
+    "revision": "ab8324974784bdc1fcdd",
+    "url": "/trek-oz_series/js/app.b3d33f88.js"
+  },
+  {
+    "revision": "8d59e1ccb2e346acbb0b",
+    "url": "/trek-oz_series/js/chunk-2c8b44e9.0f8b84b5.js"
+  },
+  {
+    "revision": "d31e1b2be5f968db6ea4",
+    "url": "/trek-oz_series/js/chunk-8af573d2.5a19ea23.js"
+  },
+  {
+    "revision": "60747180cb7fcbe2cc05",
+    "url": "/trek-oz_series/js/chunk-9b4cece0.7a5c73a9.js"
+  },
+  {
+    "revision": "7ff63135c0cb7e4b15f6",
+    "url": "/trek-oz_series/js/chunk-vendors.2b8a3fac.js"
+  },
+  {
+    "revision": "d7c9768f9f7d8f28b7a534b1b110068a",
+    "url": "/trek-oz_series/manifest.json"
+  },
+  {
+    "revision": "60b62c962668fa095d8f15a8817bde87",
+    "url": "/trek-oz_series/pages/offline.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/trek-oz_series/robots.txt"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  // publicPath: '/trek-oz_series/',
   transpileDependencies: [
     'vuetify',
   ],
