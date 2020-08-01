0ad20cd4e3c41b42a3b3daa51e243defae894583
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-08-01 19:12:11 -0300

    Creates header component to details page

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-img
+    :src="'https://i1.wp.com/elrincon.tv/wp-content/uploads/2014/02/psych.jpg'"
+    :height="height"
+    :gradient="gradientOverlay"
+    scroll-target="#scroll-target"
+  >
+    <v-row align="center" class="app-bar-height white--text py-0 px-6">
+      <div>
+        <v-btn
+          icon
+          @click="handleBackButton"
+        >
+          <arrow-left-icon class="headline white--text font-weight-bold" />
+        </v-btn>
+      </div>
+    </v-row>
+    <v-row align="end" :class="headerHeight" class="white--text pt-2 pb-0 px-6">
+      <div class="px-3">
+        <div class="headline font-weight-bold">{{ title }}</div>
+        <div v-if="showSubtitle && !!subtitle" class="subheading">{{ subtitle }}</div>
+      </div>
+    </v-row>
+  </v-img>
+</template>
+
+<script>
+import {
+  ArrowLeftIcon,
+} from 'vue-feather-icons';
+
+export default {
+  components: {
+    ArrowLeftIcon,
+  },
+
+  props: {
+    height: {
+      type: String,
+      default: '180px',
+    },
+    title: {
+      type: String,
+      default: null,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      default: null,
+    },
+  },
+
+  data() {
+    return {
+      gradientOverlay: 'to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)',
+    };
+  },
+
+  methods: {
+    handleBackButton() {
+      this.$emit('back', true);
+    },
+  },
+
+  computed: {
+    headerHeight() {
+      if (this.height === '130px') {
+        return 'h-48-px';
+      }
+      return 'h-100-px';
+    },
+
+    showSubtitle() {
+      return !(this.height === '130px');
+    },
+
+    expandedHeight() {
+      return this.height === '130px' ? 'expanded-body' : '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.app-bar-height {
+  height: 56px;
+}
+
+.h-100-px {
+  height: 100px;
+}
+
+.h-48-px {
+  height: 48px;
+}
+</style>
--- src/views/Details.vue
+++ src/views/Details.vue
@@ -2,101 +2,106 @@
   <main-wrapper
     no-menu
   >
-    <v-img
-      :src="'https://i1.wp.com/elrincon.tv/wp-content/uploads/2014/02/psych.jpg'"
-      height="180px"
-      class="mb-n4"
-    />
-    <v-card
-      class="px-3 pt-2"
-      rounded="xl"
-    >
-      <v-card-title class="headline primary--text font-weight-bold">Psych</v-card-title>
-      <v-card-subtitle>
-        <v-row>
-          <v-col
-            class="pb-1 pt-0"
-            cols="6"
-            md="3"
+    <v-card class="overflow-hidden">
+      <app-header
+        title="Psych"
+        subtitle="4ª Temporada"
+        :height="headerHeight"
+        @back="returnToPreviousPage"
+      />
+      <v-card
+        class="px-3 pt-2 overflow-y-auto card-max-height mt-n4"
+        :class="expandedHeight"
+        rounded="t-xl"
+        id="scroll-target"
+        v-scroll.self="onScroll"
+      >
+        <v-card-subtitle>
+          <v-row>
+            <v-col
+              class="pb-1 pt-0"
+              cols="6"
+              md="3"
+            >
+              <span class="caption text-uppercase"> Gênero: </span><br>
+              Ação, Aventura, Comédia
+            </v-col>
+            <v-col
+              class="pb-1 pt-0"
+              cols="6"
+              md="3"
+            >
+              <span class="caption text-uppercase"> Status: </span><br>
+              Cancelada
+            </v-col>
+            <v-col
+              class="pb-1 pt-0"
+              cols="6"
+              md="3"
+            >
+              <span class="caption text-uppercase"> Estreia: </span><br>
+              25 de setembro de 2007
+            </v-col>
+            <v-col
+              class="pb-1 pt-0"
+              cols="6"
+              md="3"
+            >
+              <span class="caption text-uppercase"> Último episódio: </span><br>
+              21 de março de 2012
+            </v-col>
+            <v-col
+              class="pb-1 pt-0"
+              cols="6"
+              md="3"
+            >
+              <span class="caption text-uppercase"> Duração do episódio: </span><br>
+              40 min
+            </v-col>
+            <v-col
+              class="pb-1 pt-0"
+              cols="6"
+              md="3"
+            >
+              <span class="caption text-uppercase"> Total de Episódios: </span><br>
+              91 em 5 temporadas
+            </v-col>
+          </v-row>
+        </v-card-subtitle>
+        <v-card-text>
+          <div
+            class="pt-3"
           >
-            <span class="caption text-uppercase"> Gênero: </span><br>
-            Ação, Aventura, Comédia
-          </v-col>
-          <v-col
-            class="pb-1 pt-0"
-            cols="6"
-            md="3"
+            <p
+              class="mx-3 mb-3 caption pr-3 text-end text-uppercase"
+            >
+              Atores
+            </p>
+            <v-divider></v-divider>
+          </div>
+          <div
+            class="pt-3"
           >
-            <span class="caption text-uppercase"> Status: </span><br>
-            Cancelada
-          </v-col>
-          <v-col
-            class="pb-1 pt-0"
-            cols="6"
-            md="3"
+            <p
+              class="mx-3 mb-3 caption pr-3 text-end text-uppercase"
+            >
+              Temporadas
+            </p>
+            <v-divider></v-divider>
+          </div>
+          <div
+            class="pt-3"
           >
-            <span class="caption text-uppercase"> Estreia: </span><br>
-            25 de setembro de 2007
-          </v-col>
-          <v-col
-            class="pb-1 pt-0"
-            cols="6"
-            md="3"
-          >
-            <span class="caption text-uppercase"> Último episódio: </span><br>
-            21 de março de 2012
-          </v-col>
-          <v-col
-            class="pb-1 pt-0"
-            cols="6"
-            md="3"
-          >
-            <span class="caption text-uppercase"> Duração do episódio: </span><br>
-            40 min
-          </v-col>
-          <v-col
-            class="pb-1 pt-0"
-            cols="6"
-            md="3"
-          >
-            <span class="caption text-uppercase"> Total de Episódios: </span><br>
-            91 em 5 temporadas
-          </v-col>
-        </v-row>
-      </v-card-subtitle>
-      <v-card-text>
-        <div
-          class="pt-3"
-        >
-          <p
-            class="mx-3 mb-3 caption pr-3 text-end text-uppercase"
-          >
-            Atores
-          </p>
-          <v-divider></v-divider>
-        </div>
-        <div
-          class="pt-3"
-        >
-          <p
-            class="mx-3 mb-3 caption pr-3 text-end text-uppercase"
-          >
-            Temporadas
-          </p>
-          <v-divider></v-divider>
-        </div>
-        <div
-          class="pt-3"
-        >
-          <p
-            class="mx-3 mb-3 caption pr-3 text-end text-uppercase"
-          >
-            Recomendações
-          </p>
-          <v-divider></v-divider>
-        </div>
-        <br><br><br>
-      </v-card-text>
+            <p
+              class="mx-3 mb-3 caption pr-3 text-end text-uppercase"
+            >
+              Recomendações
+            </p>
+            <v-divider></v-divider>
+          </div>
+          <br><br><br>
+        </v-card-text>
+      </v-card>
     </v-card>
     <v-row
       v-if="loading"
@@ -126,23 +131,49 @@ import {
 import {
 //
 } from 'vue-feather-icons';
+import AppHeader from '../components/AppHeader.vue';
 
 export default {
   components: {
-    //
+    AppHeader,
   },
 
   data() {
     return {
-      search: '',
-      cards: [],
       loading: false,
       error: false,
+      headerHeight: '180px',
     };
   },
 
   methods: {
-    //
+    onScroll(e) {
+      if (e.target.scrollTop > 130) {
+        this.headerHeight = '130px';
+        return;
+      }
+      this.headerHeight = '180px';
+    },
+
+    returnToPreviousPage() {
+      console.log('Return to previous page');
+    },
+  },
+
+  computed: {
+    expandedHeight() {
+      return this.headerHeight === '130px' ? 'expanded-body' : '';
+    },
   },
 };
 </script>
+
+<style scoped>
+.card-max-height {
+  max-height: calc(100vh - 180px + 16px);
+}
+
+.card-max-height.expanded-body {
+  max-height: calc(100vh - 130px + 16px);
+}
+</style>
