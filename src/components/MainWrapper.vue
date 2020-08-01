0eeb20f3b92b6cc98eb3dada577c0b83508a7d74
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-08-01 10:44:15 -0300

    Creates an main wrapper with navigation menu and footer

---
+++ src/components/MainWrapper.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-app>
+      <v-main
+        class="pb-main"
+      >
+        <slot/>
+      </v-main>
+    </v-app>
+    <nav-bar v-if="!noMenu"/>
+  </div>
+</template>
+
+<script>
+import NavBar from './NavBar.vue';
+
+export default {
+  components: {
+    NavBar,
+  },
+
+  props: {
+    noMenu: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style>
+.pb-main {
+  padding-bottom: 7.5rem !important;
+}
+</style>
