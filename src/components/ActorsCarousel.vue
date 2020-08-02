23edc3c528dd46036849d63f0316623a064ab202
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-08-02 11:54:47 -0300

    Add actors section component

---
+++ src/components/ActorsCarousel.vue
@@ -0,0 +1,131 @@
+<template>
+  <span>
+    <slick
+      v-bind="slickOptions"
+      class="mb-8"
+    >
+      <v-col
+        v-for="(actor, index) in actors"
+        :key="index"
+        class="d-flex justify-center px-0 pb-5"
+      >
+        <div>
+          <p
+            class="text--disabled mb-0 mr-1 text-end">
+            {{ REGULARITY_TEXT[actor.regularity] }}
+          </p>
+          <v-img
+            :src="actor.src"
+            width="100"
+            height="130"
+            class="mb-2"
+          />
+          <span
+            class="subtitle-2 black--text d-inline-block text-truncate max-w-105"
+          >
+            {{ actor.name }}
+          </span><br>
+          <span
+            class="body-2 d-inline-block text-truncate max-w-105"
+          >
+            {{ actor.character }}
+          </span><br>
+        </div>
+      </v-col>
+    </slick>
+  </span>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import { REGULARITY_TEXT } from '../constants/regularity';
+
+export default {
+  components: {
+    Slick: VueSlickCarousel,
+  },
+
+  props: {
+    actors: {
+      type: Array,
+      default: () => ([]),
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      REGULARITY_TEXT,
+      slickOptions: {
+        dots: true,
+        focusOnSelect: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 7,
+        slidesToScroll: 6,
+        touchThreshold: 5,
+        responsive: [
+          {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 6,
+              slidesToScroll: 5,
+            },
+          },
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 4,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 3,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            breakpoint: 380,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 270,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+.max-w-105 {
+  max-width: 105px;
+}
+
+.subtitle-2, .body-2 {
+  line-height: 1.075;
+}
+
+.slick-slide div:focus {
+  outline: none;
+}
+</style>
--- src/views/Details.vue
+++ src/views/Details.vue
@@ -78,6 +78,9 @@
               Atores
             </p>
             <v-divider></v-divider>
+            <actors-carousel
+              :actors="actors"
+            />
           </div>
           <div
             class="pt-3"
@@ -132,10 +135,12 @@ import {
 //
 } from 'vue-feather-icons';
 import AppHeader from '../components/AppHeader.vue';
+import ActorsCarousel from '../components/ActorsCarousel.vue';
 
 export default {
   components: {
     AppHeader,
+    ActorsCarousel,
   },
 
   data() {
@@ -143,6 +148,50 @@ export default {
       loading: false,
       error: false,
       headerHeight: '180px',
+      actors: [
+        {
+          name: 'James Roday',
+          character: 'Shawn Spencer',
+          regularity: 'regular', // recurring, guest
+          src: 'https://image.tmdb.org/t/p/w138_and_h175_face/1iRFSkNsB45BHUMyWww2h6K9B8Z.jpg',
+        },
+        {
+          name: 'Dulé Hill',
+          character: 'Burton Guster',
+          regularity: 'regular', // recurring, guest
+          src: 'https://image.tmdb.org/t/p/w138_and_h175_face/fPJ8lIx6WwBgO2AhnHQSeiZiAUy.jpg',
+        },
+        {
+          name: 'Timothy Omundson',
+          character: 'Carlton Lassiter',
+          regularity: 'regular', // recurring, guest
+          src: 'https://image.tmdb.org/t/p/w138_and_h175_face/fPhLMiodU7mVegOYvJ8aoj84ZHJ.jpg',
+        },
+        {
+          name: 'Maggie Lawson',
+          character: 'Juliet O\'Hara',
+          regularity: 'regular', // recurring, guest
+          src: 'https://image.tmdb.org/t/p/w138_and_h175_face/9AZEDpnDrOMkCf9hqWhY4szrv3X.jpg',
+        },
+        {
+          name: 'Kirsten Nelson',
+          character: 'Karen Vick',
+          regularity: 'regular', // recurring, guest
+          src: 'https://image.tmdb.org/t/p/w138_and_h175_face/7TfweasXJLw5cKNAiV1xYzIJYcT.jpg',
+        },
+        {
+          name: 'Corbin Bernsen',
+          character: 'Henry Spencer',
+          regularity: 'regular', // recurring, guest
+          src: 'https://image.tmdb.org/t/p/w138_and_h175_face/mLftTYHrvct0VBB8pGhJjLHZUXQ.jpg',
+        },
+        {
+          name: 'Sage Brocklebank',
+          character: 'Buzz McNab',
+          regularity: 'recurring',
+          src: 'https://image.tmdb.org/t/p/w138_and_h175_face/7BFFanLsfc2m4FU7Hvw8GDTGOEt.jpg',
+        },
+      ],
     };
   },
 
