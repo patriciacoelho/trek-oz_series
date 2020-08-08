fa19995adf6e7562848cde73be134351bb1ed06f
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-08-08 13:20:37 -0300

    Add services to request TV Show details

--- src/services.js
+++ src/services.js
@@ -1,7 +1,24 @@
-import { trakt, tmdb } from './api';
+import {
+  trakt,
+  tmdb,
+//   fanart,
+} from './api';
 
+// ...................
+// *** TRAKT API ***
+// ...................
 export const getSeriesBySearch = (query) => trakt.get('/search/show', { params: { query } }).then(({ data }) => data);
+export const getSerieSummary = (id) => trakt.get(`/shows/${id}?extended=full`).then(({ data }) => data);
 
-export const getSerie = (id) => trakt.get(`/search/trakt/${id}?type=show&extended=full`).then(({ data }) => data);
-
+// ...................
+// *** TMDB API ***
+// ...................
 export const getSeriePosters = (id) => tmdb.get(`/tv/${id}/images?api_key=${process.env.VUE_APP_TMDB_API_KEY_V3}`).then(({ data }) => data.posters);
+export const getSerieDetails = (id) => tmdb.get(`/tv/${id}?api_key=${process.env.VUE_APP_TMDB_API_KEY_V3}&language=pt-BR`).then(({ data }) => data);
+export const getSerieCredits = (id) => tmdb.get(`/tv/${id}/credits?api_key=${process.env.VUE_APP_TMDB_API_KEY_V3}&language=pt-BR`).then(({ data }) => data);
+
+// ...................
+// *** FANART API ***
+// ...................
+// export const getSeriePosterFanart = (id)
+// => fanart.get(`/tv/${id}`).then(({ data }) => data.tvposter);
