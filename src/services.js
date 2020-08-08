1f0d37d5f8a8d27d9d22068a8516907b39c4eceb
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-08-08 18:09:47 -0300

    Add request to Season details

--- src/services.js
+++ src/services.js
@@ -16,6 +16,7 @@ export const getSerieSummary = (id) => trakt.get(`/shows/${id}?extended=full`).t
 export const getSeriePosters = (id) => tmdb.get(`/tv/${id}/images?api_key=${process.env.VUE_APP_TMDB_API_KEY_V3}`).then(({ data }) => data.posters);
 export const getSerieDetails = (id) => tmdb.get(`/tv/${id}?api_key=${process.env.VUE_APP_TMDB_API_KEY_V3}&language=pt-BR`).then(({ data }) => data);
 export const getSerieCredits = (id) => tmdb.get(`/tv/${id}/credits?api_key=${process.env.VUE_APP_TMDB_API_KEY_V3}&language=pt-BR`).then(({ data }) => data);
+export const getSerieSeason = (id, N) => tmdb.get(`/tv/${id}/season/${N}?api_key=${process.env.VUE_APP_TMDB_API_KEY_V3}&language=pt-BR`).then(({ data }) => data);
 
 // ...................
 // *** FANART API ***
