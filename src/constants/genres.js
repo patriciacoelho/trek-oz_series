8707bccf14ece4590ba7c54ec838f59a87f3a023
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-08-11 23:41:23 -0300

    Add constants file to translate some genres

---
+++ src/constants/genres.js
@@ -0,0 +1,13 @@
+export const GENRES = {
+  ACTION_ADVENTURE: 'Action & Adventure',
+  KIDS: 'Kids',
+  SCI_FI_FANTASY: 'Sci-Fi & Fantasy',
+  WAR_POLITICS: 'War & Politics',
+};
+
+export const GENRES_TRANSLATION = {
+  [GENRES.ACTION_ADVENTURE]: 'Ação e Aventura',
+  [GENRES.KIDS]: 'Infantil',
+  [GENRES.SCI_FI_FANTASY]: 'Ficção Cientifica e Fantasia',
+  [GENRES.WAR_POLITICS]: 'Guerra e Política',
+};
