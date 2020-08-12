72379626ff0f07e3429b37d09b50114daeb85153
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-08-11 23:59:01 -0300

    Add array with all genres in constants file

--- src/constants/genres.js
+++ src/constants/genres.js
@@ -3,6 +3,18 @@ export const GENRES = {
   KIDS: 'Kids',
   SCI_FI_FANTASY: 'Sci-Fi & Fantasy',
   WAR_POLITICS: 'War & Politics',
+  ANIMATION: 'Animation',
+  COMEDY: 'Comedy',
+  CRIME: 'Crime',
+  DOCUMENTARY: 'Documentary',
+  DRAMA: 'Drama',
+  FAMILY: 'Family',
+  MYSTERY: 'Mystery',
+  NEWS: 'News',
+  REALITY: 'Reality',
+  SOAP: 'Soap',
+  TALK: 'Talk',
+  WESTERN: 'Western',
 };
 
 export const GENRES_TRANSLATION = {
@@ -11,3 +23,70 @@ export const GENRES_TRANSLATION = {
   [GENRES.SCI_FI_FANTASY]: 'Ficção Cientifica e Fantasia',
   [GENRES.WAR_POLITICS]: 'Guerra e Política',
 };
+
+export const ALL_GENRES = [
+  {
+    id: 10759,
+    name: GENRES.ACTION_ADVENTURE,
+  },
+  {
+    id: 16,
+    name: GENRES.ANIMATION,
+  },
+  {
+    id: 35,
+    name: GENRES.COMEDY,
+  },
+  {
+    id: 80,
+    name: GENRES.CRIME,
+  },
+  {
+    id: 99,
+    name: GENRES.DOCUMENTARY,
+  },
+  {
+    id: 18,
+    name: GENRES.DRAMA,
+  },
+  {
+    id: 10751,
+    name: GENRES.FAMILY,
+  },
+  {
+    id: 10762,
+    name: GENRES.KIDS,
+  },
+  {
+    id: 9648,
+    name: GENRES.MYSTERY,
+  },
+  {
+    id: 10763,
+    name: GENRES.NEWS,
+  },
+  {
+    id: 10764,
+    name: GENRES.REALITY,
+  },
+  {
+    id: 10765,
+    name: GENRES.SCI_FI_FANTASY,
+  },
+  {
+    id: 10766,
+    name: GENRES.SOAP,
+  },
+  {
+    id: 10767,
+    name: GENRES.TALK,
+  },
+  {
+    id: 10768,
+    name: GENRES.WAR_POLITICS,
+  },
+  {
+    id: 37,
+    name: GENRES.WESTERN,
+  },
+];
