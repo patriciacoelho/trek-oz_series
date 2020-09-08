b8df8637f2f7da767e6997005fff8c6bdb391946
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-09-08 11:29:25 -0300

    Update '/docs'

--- docs/precache-manifest.c14707d0cf7cceec3c7dfe3fcf964075.js
+++ docs/precache-manifest.c14707d0cf7cceec3c7dfe3fcf964075.js
@@ -1,47 +1,47 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "803c19ccb5a3c6ee1744",
-    "url": "/trek-oz_series/css/app.93fc59c4.css"
+    "revision": "3403ef51fdbf45148736",
+    "url": "/trek-oz_series/css/app.7bc610f7.css"
   },
   {
-    "revision": "bf2d6bfc2047d5568ded",
-    "url": "/trek-oz_series/css/chunk-27def578.4e9dc113.css"
+    "revision": "c0359ae246d1c0acf489",
+    "url": "/trek-oz_series/css/chunk-43122ddc.42de575a.css"
   },
   {
-    "revision": "842b543557f8b99967d6",
+    "revision": "649428ce9b7db72bebca",
     "url": "/trek-oz_series/css/chunk-522e1525.f74cf833.css"
   },
   {
-    "revision": "d618ccfcf138fa1e678b",
+    "revision": "24ba708e071b394a8c67",
     "url": "/trek-oz_series/css/chunk-56b15d95.c37776db.css"
   },
   {
-    "revision": "d05382e31a90119583fe",
+    "revision": "2c38dc1f6f36a5362e9f",
     "url": "/trek-oz_series/css/chunk-5b8b5eda.1b65b310.css"
   },
   {
-    "revision": "6b2fd17ece0c1940297a",
+    "revision": "0fae3013d2d94a109f90",
     "url": "/trek-oz_series/css/chunk-5c24050b.bf91edcd.css"
   },
   {
-    "revision": "04e66cf3f0f5d478df7a",
+    "revision": "ce967c4c08a818b2e6db",
     "url": "/trek-oz_series/css/chunk-649cf68b.c9a78054.css"
   },
   {
-    "revision": "8570d416246854667326",
+    "revision": "c8844a12a266b04dcaee",
     "url": "/trek-oz_series/css/chunk-66af710a.77fd9e0a.css"
   },
   {
-    "revision": "951100915d39a728694e",
-    "url": "/trek-oz_series/css/chunk-a2ef258e.ddb05a8c.css"
+    "revision": "fd3ba2937542dd4a912a",
+    "url": "/trek-oz_series/css/chunk-6c1bc712.ddb05a8c.css"
   },
   {
     "revision": "74ef671d0709132382a6",
     "url": "/trek-oz_series/css/chunk-f4af00d8.654f70e7.css"
   },
   {
-    "revision": "c358b639e0ee33ddd1c2",
-    "url": "/trek-oz_series/css/chunk-vendors.5728ee34.css"
+    "revision": "b8b048f2e6dda58e57ae",
+    "url": "/trek-oz_series/css/chunk-vendors.489235a2.css"
   },
   {
     "revision": "6e6f4fdc1537afa8bb711df7cbaa892c",
@@ -76,52 +76,52 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/trek-oz_series/img/trakt-wide-red-black.e5b42e75.svg"
   },
   {
-    "revision": "63084fe2ad4b969637e855b73f456b76",
+    "revision": "12dd9c74a9ebdad7910b2544ca39219e",
     "url": "/trek-oz_series/index.html"
   },
   {
-    "revision": "803c19ccb5a3c6ee1744",
-    "url": "/trek-oz_series/js/app.16129b35.js"
+    "revision": "3403ef51fdbf45148736",
+    "url": "/trek-oz_series/js/app.76f91c2a.js"
   },
   {
-    "revision": "bf2d6bfc2047d5568ded",
-    "url": "/trek-oz_series/js/chunk-27def578.dd92250a.js"
+    "revision": "c0359ae246d1c0acf489",
+    "url": "/trek-oz_series/js/chunk-43122ddc.7618df1c.js"
   },
   {
-    "revision": "842b543557f8b99967d6",
-    "url": "/trek-oz_series/js/chunk-522e1525.3f286db4.js"
+    "revision": "649428ce9b7db72bebca",
+    "url": "/trek-oz_series/js/chunk-522e1525.7ba3e7e6.js"
   },
   {
-    "revision": "d618ccfcf138fa1e678b",
-    "url": "/trek-oz_series/js/chunk-56b15d95.dab71f90.js"
+    "revision": "24ba708e071b394a8c67",
+    "url": "/trek-oz_series/js/chunk-56b15d95.afa0401a.js"
   },
   {
-    "revision": "d05382e31a90119583fe",
-    "url": "/trek-oz_series/js/chunk-5b8b5eda.7a67cd52.js"
+    "revision": "2c38dc1f6f36a5362e9f",
+    "url": "/trek-oz_series/js/chunk-5b8b5eda.1da8f9b4.js"
   },
   {
-    "revision": "6b2fd17ece0c1940297a",
-    "url": "/trek-oz_series/js/chunk-5c24050b.d023b225.js"
+    "revision": "0fae3013d2d94a109f90",
+    "url": "/trek-oz_series/js/chunk-5c24050b.0de37d82.js"
   },
   {
-    "revision": "04e66cf3f0f5d478df7a",
-    "url": "/trek-oz_series/js/chunk-649cf68b.50e7ec7a.js"
+    "revision": "ce967c4c08a818b2e6db",
+    "url": "/trek-oz_series/js/chunk-649cf68b.6dfbe399.js"
   },
   {
-    "revision": "8570d416246854667326",
-    "url": "/trek-oz_series/js/chunk-66af710a.71104251.js"
+    "revision": "c8844a12a266b04dcaee",
+    "url": "/trek-oz_series/js/chunk-66af710a.edb8d553.js"
   },
   {
-    "revision": "951100915d39a728694e",
-    "url": "/trek-oz_series/js/chunk-a2ef258e.103231ab.js"
+    "revision": "fd3ba2937542dd4a912a",
+    "url": "/trek-oz_series/js/chunk-6c1bc712.b85d8a5a.js"
   },
   {
     "revision": "74ef671d0709132382a6",
     "url": "/trek-oz_series/js/chunk-f4af00d8.fd86d741.js"
   },
   {
-    "revision": "c358b639e0ee33ddd1c2",
-    "url": "/trek-oz_series/js/chunk-vendors.184fdb20.js"
+    "revision": "b8b048f2e6dda58e57ae",
+    "url": "/trek-oz_series/js/chunk-vendors.46206b57.js"
   },
   {
     "revision": "d7c9768f9f7d8f28b7a534b1b110068a",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/trek-oz_series/precache-manifest.6a76d871bdb1d65110b1ba1353c8b046.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/trek-oz_series/precache-manifest.c14707d0cf7cceec3c7dfe3fcf964075.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable */
 /* eslint-disable no-undef */
